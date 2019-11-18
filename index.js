6eea0a125d7e870447bf7e0b534cf681ade1fc99
gnardinosaur <github email address>
2019-11-18 09:52:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file.
+function lowerCaseDrivers(arr) {
+    let newArr = [];
+    arr.map(function(el) {
+        newArr.push(el.toLowerCase());
+    });
+    return newArr;
+}
+
+function nameToAttributes(arr) {
+    return arr.map(function(el) {
+        let fullName = el.split(" ")
+        return { firstName: fullName[0], lastName: fullName[1] };
+    });
+}
+
+function attributesToPhrase(arr) {
+    let newArr = [];
+    arr.map(function(el){
+        newArr.push(`${el.name} is from ${el.hometown}`);
+    })
+    return newArr;
+}
+
